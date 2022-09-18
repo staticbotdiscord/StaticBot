f1950b6d5ece7682c9d939817bf3b057fe793432
Noinkin <87577662+Noinkin@users.noreply.github.com>
2022-09-18 21:31:10 +0100

    Fix: Ban.js and Kick.js Permissions
    Change PermissionsFlagsBits to PermissionFlagsBits

--- src/commands/moderator/ban.js
+++ src/commands/moderator/ban.js
@@ -1,4 +1,4 @@
-const { SlashCommandBuilder, PermissionsFlagsBits } = require('discord.js')
+const { SlashCommandBuilder, PermissionFlagsBits } = require('discord.js')
 
 module.exports = {
 	data: new SlashCommandBuilder()
@@ -11,7 +11,7 @@ module.exports = {
 			fr: 'Kick le joueur spécifié.'
 		})
 		.setDMPermission(false)
-		.setDefaultMemberPermissions(PermissionsFlagsBits.BanMembers)
+		.setDefaultMemberPermissions(PermissionsFlagBits.BanMembers)
 		.addUserOption(option =>
 			option.setName('user')
 				.setNameLocalizations({
