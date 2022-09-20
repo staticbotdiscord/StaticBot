c4f7a374a70d4230e89758563fd47e7b6e12e77f
Noinkin <87577662+Noinkin@users.noreply.github.com>
2022-09-20 17:52:30 +0100

    chore: Format all files

--- src/commands/moderator/ban.js
+++ src/commands/moderator/ban.js
@@ -1,106 +1,121 @@
 const {
-  SlashCommandBuilder,
-  PermissionFlagsBits,
-  PermissionsBitField,
-  EmbedBuilder,
-} = require("discord.js");
+    SlashCommandBuilder,
+    PermissionFlagsBits,
+    PermissionsBitField,
+    EmbedBuilder,
+} = require('discord.js');
 
 module.exports = {
-  data: new SlashCommandBuilder()
-    .setName("ban")
-    .setNameLocalizations({
-      fr: "interdire",
-    })
-    .setDescription("Bans the player specified.")
-    .setDescriptionLocalizations({
-      fr: "Kick le joueur spécifié.",
-    })
-    .setDMPermission(false)
-    .setDefaultMemberPermissions(PermissionFlagsBits.BanMembers)
-    .addUserOption((option) =>
-      option
-        .setName("user")
+    data: new SlashCommandBuilder()
+        .setName('ban')
         .setNameLocalizations({
-          fr: "utilisateur",
+            fr: 'interdire',
         })
-        .setDescription("The user to kick.")
+        .setDescription('Bans the player specified.')
         .setDescriptionLocalizations({
-          fr: "L'utilisateur à frapper.",
+            fr: 'Kick le joueur spécifié.',
         })
-        .setRequired(true)
-    )
-    .addStringOption((option) =>
-      option
-        .setName("reason")
-        .setNameLocalizations({
-          fr: "raison",
-        })
-        .setDescription("Reason for the kick.")
-        .setDescriptionLocalizations({
-          fr: "La raison du coup de pied.",
-        })
-        .setRequired(false)
-    ),
-  ephemeral: false,
-  async execute(client, interaction, options) {
-    const locales2 = {
-      fr: "Je n'ai pas la permission d'exclure des membres",
-    };
-    const embed2 = new EmbedBuilder()
-      .setDescription(
-        locales2[interaction.locale] ?? "I don't have permission to ban members"
-      )
-      .setColor("#c71212");
-    if (
-      !interaction.guild.members.me.permissions.has(
-        PermissionsBitField.Flags.BanMembers
-      )
-    )
-      return await interaction.editReply({ content: null, embeds: [embed2] });
-    const memberPosition =
-      interaction.options.getMember("user").roles?.highest.rawPosition;
-    const moderationPosition = interaction.member.roles?.highest.rawPosition;
-    const myPosition = interaction.guild.member.me.roles?.highest.rawPosition;
-    const locales3 = {
-      fr: "Votre rôle n'est pas supérieur au rôle de la personne que vous essayez de modérer",
-    };
-    const embed3 = new EmbedBuilder()
-      .setDescription(
-        locales3[interaction.locale] ??
-          "Your role is not higher than the person you are trying to moderate's role"
-      )
-      .setColor("#c71212");
-    if (moderationPosition <= memberPosition)
-      return await interaction.editReply({ content: null, embeds: [embed3] });
-    const locales4 = {
-      fr: "Mon rôle le plus élevé n'est pas supérieur au rôle le plus élevé de la personne que vous essayez de modérer",
-    };
-    const embed4 = new EmbedBuilder()
-      .setDescription(
-        locales4[interaction.locale] ??
-          "My highest role is not higher than the person you are trying to moderate's highest role"
-      )
-      .setColor("#c71212");
-    if (myPosition <= memberPosition)
-      return await interaction.editReply({ content: null, embeds: [embed4] });
-    const locales = {
-      fr: "Coup de pied pour!" + interaction.options.getMember("user") + "!",
-    };
-    const locales1 = {
-      fr: "Demandé par: ",
-    };
-    const embed = new EmbedBuilder()
-      .setDescription(
-        locales[interaction.locale] ??
-          "Kicked" + interaction.options.getMember("user") + "!"
-      )
-      .setColor("05c200");
-    const reason =
-      interaction.options.getString("reason") ?? "No reason specified";
-    const request = locales1[interaction.locale] ?? "Requested by: ";
-    interaction.options
-      .getMember("user")
-      .kick(locales1 + interaction.member.tag + " : " + reason);
-    await interaction.editReply({ content: null, embeds: [embed] });
-  },
+        .setDMPermission(false)
+        .setDefaultMemberPermissions(PermissionFlagsBits.BanMembers)
+        .addUserOption((option) =>
+            option
+                .setName('user')
+                .setNameLocalizations({
+                    fr: 'utilisateur',
+                })
+                .setDescription('The user to kick.')
+                .setDescriptionLocalizations({
+                    fr: "L'utilisateur à frapper.",
+                })
+                .setRequired(true),
+        )
+        .addStringOption((option) =>
+            option
+                .setName('reason')
+                .setNameLocalizations({
+                    fr: 'raison',
+                })
+                .setDescription('Reason for the kick.')
+                .setDescriptionLocalizations({
+                    fr: 'La raison du coup de pied.',
+                })
+                .setRequired(false),
+        ),
+    ephemeral: false,
+    async execute(client, interaction, options) {
+        const locales2 = {
+            fr: "Je n'ai pas la permission d'exclure des membres",
+        };
+        const embed2 = new EmbedBuilder()
+            .setDescription(
+                locales2[interaction.locale] ??
+                    "I don't have permission to ban members",
+            )
+            .setColor('#c71212');
+        if (
+            !interaction.guild.members.me.permissions.has(
+                PermissionsBitField.Flags.BanMembers,
+            )
+        )
+            return await interaction.editReply({
+                content: null,
+                embeds: [embed2],
+            });
+        const memberPosition =
+            interaction.options.getMember('user').roles?.highest.rawPosition;
+        const moderationPosition =
+            interaction.member.roles?.highest.rawPosition;
+        const myPosition =
+            interaction.guild.member.me.roles?.highest.rawPosition;
+        const locales3 = {
+            fr: "Votre rôle n'est pas supérieur au rôle de la personne que vous essayez de modérer",
+        };
+        const embed3 = new EmbedBuilder()
+            .setDescription(
+                locales3[interaction.locale] ??
+                    "Your role is not higher than the person you are trying to moderate's role",
+            )
+            .setColor('#c71212');
+        if (moderationPosition <= memberPosition)
+            return await interaction.editReply({
+                content: null,
+                embeds: [embed3],
+            });
+        const locales4 = {
+            fr: "Mon rôle le plus élevé n'est pas supérieur au rôle le plus élevé de la personne que vous essayez de modérer",
+        };
+        const embed4 = new EmbedBuilder()
+            .setDescription(
+                locales4[interaction.locale] ??
+                    "My highest role is not higher than the person you are trying to moderate's highest role",
+            )
+            .setColor('#c71212');
+        if (myPosition <= memberPosition)
+            return await interaction.editReply({
+                content: null,
+                embeds: [embed4],
+            });
+        const locales = {
+            fr:
+                'Coup de pied pour!' +
+                interaction.options.getMember('user') +
+                '!',
+        };
+        const locales1 = {
+            fr: 'Demandé par: ',
+        };
+        const embed = new EmbedBuilder()
+            .setDescription(
+                locales[interaction.locale] ??
+                    'Kicked' + interaction.options.getMember('user') + '!',
+            )
+            .setColor('05c200');
+        const reason =
+            interaction.options.getString('reason') ?? 'No reason specified';
+        const request = locales1[interaction.locale] ?? 'Requested by: ';
+        interaction.options
+            .getMember('user')
+            .kick(locales1 + interaction.member.tag + ' : ' + reason);
+        await interaction.editReply({ content: null, embeds: [embed] });
+    },
 };
--- src/events/interactionCreate.js
+++ src/events/interactionCreate.js
@@ -1,54 +1,58 @@
-const { thinkingMessage, thinkingMessageLocales } = require("../../config");
+const { thinkingMessage, thinkingMessageLocales } = require('../../config');
 
 module.exports = {
-  name: "interactionCreate",
-  async execute(interaction) {
-    const client = interaction.client;
-    if (interaction.isChatInputCommand()) {
-      const command = client.commands.get(interaction.commandName);
+    name: 'interactionCreate',
+    async execute(interaction) {
+        const client = interaction.client;
+        if (interaction.isChatInputCommand()) {
+            const command = client.commands.get(interaction.commandName);
 
-      if (!command)
-        return await interaction.reply({
-          content: "There was an error while executing this command!",
-          ephemeral: true,
-        });
+            if (!command)
+                return await interaction.reply({
+                    content: 'There was an error while executing this command!',
+                    ephemeral: true,
+                });
 
-      const components = [];
-      const staff2 = require("../../lib/events/interaction/staff/staffButton1")(
-        interaction
-      );
-      if (staff2.isStaff === true) {
-        components.push(staff2.row);
-      }
-      const staff1 = require("../../lib/events/interaction/staff/staffButton")(
-        interaction
-      );
-      if (staff1.isStaff === true) {
-        components.push(staff1.row);
-      }
-      if (command.ephemeral === true) {
-        await interaction.reply({
-          content:
-            thinkingMessageLocales[interaction.locale] ?? thinkingMessage,
-          ephemeral: true,
-        });
-      } else {
-        await interaction.reply({
-          content:
-            thinkingMessageLocales[interaction.locale] ?? thinkingMessage,
-          ephemeral: false,
-          components: components,
-        });
-      }
-      try {
-        await command.execute(client, interaction);
-      } catch (error) {
-        console.log(error);
-        await interaction.editReply({
-          content: "There was an error while executing this command!",
-          ephemeral: true,
-        });
-      }
-    }
-  },
+            const components = [];
+            const staff2 =
+                require('../../lib/events/interaction/staff/staffButton1')(
+                    interaction,
+                );
+            if (staff2.isStaff === true) {
+                components.push(staff2.row);
+            }
+            const staff1 =
+                require('../../lib/events/interaction/staff/staffButton')(
+                    interaction,
+                );
+            if (staff1.isStaff === true) {
+                components.push(staff1.row);
+            }
+            if (command.ephemeral === true) {
+                await interaction.reply({
+                    content:
+                        thinkingMessageLocales[interaction.locale] ??
+                        thinkingMessage,
+                    ephemeral: true,
+                });
+            } else {
+                await interaction.reply({
+                    content:
+                        thinkingMessageLocales[interaction.locale] ??
+                        thinkingMessage,
+                    ephemeral: false,
+                    components: components,
+                });
+            }
+            try {
+                await command.execute(client, interaction);
+            } catch (error) {
+                console.log(error);
+                await interaction.editReply({
+                    content: 'There was an error while executing this command!',
+                    ephemeral: true,
+                });
+            }
+        }
+    },
 };
